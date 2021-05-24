e8c0714373d1a080838812a1ba64326482b8e6d7
Beratcan Güreş <guresberatcan@gmail.com>
2021-05-24 17:52:08 +0300

    Remembering multiple strokes done

--- app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -3,8 +3,10 @@ package com.example.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
+import java.lang.reflect.TypeVariable
 
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private var mDrawPath: CustomPath? = null
@@ -14,6 +16,9 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas: Canvas? = null
+    private var mPaths = ArrayList<CustomPath>()
+
+
 
     init {
         setUpDrawing()
@@ -27,7 +32,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -40,6 +45,13 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        for(path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPath!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+
         if (!mDrawPath!!.isEmpty) {
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color
@@ -73,7 +85,9 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
                 }
             }
             MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
+
             }
             else -> return false
         }
@@ -82,6 +96,11 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
         return true
 
+    }
+
+    fun setSizeForBrush(newSize: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
 
--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawing_view = findViewById<DrawingView>(R.id.drawing_view)
+
+        drawing_view.setSizeForBrush(20.toFloat())
+
     }
 }
