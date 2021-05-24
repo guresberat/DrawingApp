d07e1c491f08494618717133580874ad8758deef
Beratcan Güreş <guresberatcan@gmail.com>
2021-05-24 19:48:10 +0300

    Finished Different Color Ui

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.kidsdrawingapp
 import android.app.Dialog
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var ib_brush: ImageButton
     lateinit var drawing_view: DrawingView
+    lateinit var ll_paint_colors: LinearLayout
+
+    private var mImageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +22,16 @@ class MainActivity : AppCompatActivity() {
 
         drawing_view = findViewById(R.id.drawing_view)
         ib_brush = findViewById(R.id.ib_brush)
+        ll_paint_colors = findViewById(R.id.ll_paint_colors)
 
         drawing_view.setSizeForBrush(20.toFloat())
+
+        mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
+
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+        )
+
         ib_brush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
