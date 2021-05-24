82cf05a809c923034f0c330f534c4c94f4d7ebb6
Beratcan Güreş <guresberatcan@gmail.com>
2021-05-24 19:18:22 +0300

    Brush Size Completed

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,16 +1,53 @@
 package com.example.kidsdrawingapp
 
+import android.app.Dialog
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.widget.AppCompatImageButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var ib_brush: ImageButton
+    lateinit var drawing_view: DrawingView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawing_view = findViewById<DrawingView>(R.id.drawing_view)
+        drawing_view = findViewById(R.id.drawing_view)
+        ib_brush = findViewById(R.id.ib_brush)
 
         drawing_view.setSizeForBrush(20.toFloat())
+        ib_brush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
 
     }
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush Size: ")
+        val smallBtn = brushDialog.findViewById<ImageButton>(R.id.ib_small_brush)
+        smallBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val mediumBtn = brushDialog.findViewById<ImageButton>(R.id.ib_medium_brush)
+        mediumBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBtn = brushDialog.findViewById<ImageButton>(R.id.ib_large_brush)
+        largeBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+
+        brushDialog.show()
+    }
+
+
 }
